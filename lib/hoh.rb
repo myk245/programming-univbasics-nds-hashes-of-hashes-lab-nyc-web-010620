380ea3bbf62dc68c9fa4ec4c716bd7757d5654c8
Cindy Kei <myk245@nyu.edu>
2019-11-20 02:46:36 +0000

    Done.

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -13,24 +13,46 @@
 #     }
 #   }
 # }
-# 
+#
 # PORTION_2 = {
 #   label: "Order"
 # }
-# 
+#
 # PORTION_3 = {
 #   label: "Family",
 #   sub_category: {
 #     label: "Genus",
 #   }
 # }
-# 
+#
 # PORTION_4 = {
 #   label: "Species",
 #   sub_category: nil
 # }
 
 def naming_system
+  naming_system = {
+    label: "Kingdom",
+    sub_category: {
+      label: "Phylum",
+      sub_category: {
+        label: "Class",
+        sub_category: {
+          label: "Order",
+          sub_category: {
+            label: "Family",
+            sub_category: {
+              label: "Genus",
+              sub_category: {
+                label: "Species"
+                }
+              }
+            }
+          }
+         }
+       }
+     }
+
   # Remember:
   #  Kingdom
   #  Phylum
